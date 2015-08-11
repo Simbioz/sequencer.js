236754b774e4404b8aa384722c51d3575bb417fe
Michaël Fortin <fortinmike@irradiated.net>
2015-08-11 00:29:21 -0400

    Finished initial implementation of sequencer.js with ActionTask and WaitTask.

--- sequencer.js
+++ sequencer.js
@@ -3,31 +3,42 @@ var Handle = function (onRelease) {
 }
 
 var ActionTask = function (action) {
-    this.action = action;
     this.perform = function (handle) { action(); handle.release(); };
 };
 
 var WaitTask = function (duration) {
-    this.duration = duration;
     this.perform = function (handle) { setTimeout(handle.release, duration); };
 };
 
 var Sequencer = function () {
     var self = this;
+    var processing = false;
     var sequence = [];
     
     // Private functions
     
     function push(task) {
         sequence.push(task);
+        resumeProcessing();
+    }
+    
+    function resumeProcessing() {
+        if (processing) return;
+        processing = true;
         process();
     }
     
+    function finishProcessing() {
+        if (sequence.length != 0) return false;
+        processing = false;
+        return true;
+    }
+    
     function process() {
-        if (sequence.length == 0) return;
+        if (finishProcessing()) return;
         
         // When the handle is released, we'll process the next task
-        var handle = new Handle(function () { process(); });
+        var handle = new Handle(process);
         
         // Perform the next task, providing it with the handle it can
         // release to signify that its job is done
