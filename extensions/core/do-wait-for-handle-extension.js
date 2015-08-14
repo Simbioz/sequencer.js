c5e8a7d868fd0d9eda83c570e79f91063ce6e15d
Michaël Fortin <fortinmike@irradiated.net>
2015-08-13 20:38:21 -0400

    Added do-wait-for-handle-extension, which takes a handle as its only parameter and waits until that handle is released, if it's not already.

--- extensions/core/do-wait-for-handle-extension.js
+++ extensions/core/do-wait-for-handle-extension.js
@@ -1,8 +1,14 @@
-var WaitForHandleTask = function (action) {
-    this.perform = function (handle) { action(handle); };
+var DoWaitForHandleTask = function (handle) {
+    this.perform = function (sequencer_handle) {
+        if (handle.isReleased) {
+            sequencer_handle.release();
+            return;
+        }
+        handle.addOnReleaseHandler(sequencer_handle.release);
+    };
 };
 
-Sequencer.prototype.doWaitForHandle = function(action) {
-    this.push(new WaitForHandleTask(action));
+Sequencer.prototype.doWaitForHandle = function (handle) {
+    this.push(new DoWaitForHandleTask(handle));
     return this;
 };
